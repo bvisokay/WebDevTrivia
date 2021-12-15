49f83ed2e6ee0b4a1bbf4c68751630498549e62a
brandon <bvisokay@gmail.com>
2021-12-14 23:30:37 -0500

    Add search overlay

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,19 @@
 const Search = () => {
-  return <div>Search</div>
+  return (
+    <div className="search-overlay">
+      <div className="search-overlay-top shadow-sm">
+        <div className="container container--narrow">
+          <label htmlFor="live-search-field" className="search-overlay-icon">
+            <i className="fas fa-search"></i>
+          </label>
+          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+          <span className="close-live-search">
+            <i className="fas fa-times-circle"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Search
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
+//comps
+import Search from "../components/Search"
 
 const Home: NextPage = () => {
   return (
     <>
       <header>Header</header>
-      <main>Main Container</main>
+      <main>
+        <p>Main Container</p>
+        <Search />
+      </main>
       <footer>Footer</footer>
     </>
   )
