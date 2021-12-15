2e772bdf3107481f36cd180c9022466d1f28201f
brandon <bvisokay@gmail.com>
2021-12-15 00:42:53 -0500

    Initial set up of context/useReducer

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,17 +1,13 @@
+import { useContext } from "react"
+import DispatchContext from "../store/DispatchContext"
+
 const Search = () => {
+  const appWideDispatch = useContext(DispatchContext)
+
   return (
     <div className="search-overlay">
-      <div className="search-overlay-top shadow-sm">
-        <div className="container container--narrow">
-          <label htmlFor="live-search-field" className="search-overlay-icon">
-            <i className="fas fa-search"></i>
-          </label>
-          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
-          <span className="close-live-search">
-            <i className="fas fa-times-circle"></i>
-          </span>
-        </div>
-      </div>
+      <label htmlFor="live-search-field" className="search-overlay-icon"></label>
+      <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,43 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Layout from "../components/Layout"
+import { useState, useReducer } from "react"
+
+// Context Files
+import DispatchContext from "../store/DispatchContext"
+import StateContext from "../store/StateContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const initialState = {
+    loggedIn: false,
+    flashMessages: []
+  }
+
+  type ACTIONTYPES = { type: "login"; value: boolean } | { type: "logout"; value: boolean } | { type: "flashMessage"; value: any | [] }
+
+  function ourReducer(state: typeof initialState, action: ACTIONTYPES): typeof initialState {
+    switch (action.type) {
+      case "login":
+        return { loggedIn: true, flashMessages: state.flashMessages }
+      case "logout":
+        return { loggedIn: false, flashMessages: state.flashMessages }
+      case "flashMessage":
+        return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
+      default:
+        throw new Error("Bad action")
+    }
+  }
+
+  const [state, dispatch] = useReducer(ourReducer, initialState)
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   )
 }
 export default MyApp
