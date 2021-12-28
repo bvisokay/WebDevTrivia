4c1d188765de1d1f7aed4c73242ec2c3dcedfc22
brandon <bvisokay@gmail.com>
2021-12-28 18:01:37 -0500

    Swap client side page guard for getServerSideProps

--- pages/addQuestion.tsx
+++ pages/addQuestion.tsx
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react"
 import { getSession } from "next-auth/client"
+import { GetServerSideProps, GetServerSidePropsContext } from "next"
 // comps
 import NewQuestionForm from "../components/input/NewQuestionForm"
 
 const addQuestion = () => {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    getSession().then(session => {
-      if (!session) {
-        window.location.href = "/auth"
-      } else {
-        setIsLoading(false)
-      }
-    })
-  }, [])
-
-  if (isLoading) {
-    return <p>Loading...</p>
-  }
-
   // redirect away if not logged in
   return (
     <div>
       <NewQuestionForm />
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const session = await getSession({ req: context.req })
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: { session }
+  }
 }
 
 export default addQuestion
