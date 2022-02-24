06a84f7c1d5069f139ad4b0e1e78f0d3b39482b5
brandon <bvisokay@gmail.com>
2022-02-23 22:23:01 -0500

    Fix unescaped html entity?

--- pages/support.tsx
+++ pages/support.tsx
@@ -10,7 +10,7 @@ const support = () => {
         <SectionText>Is there a feature you would like to see implemented? See an Error?</SectionText>
         <SectionText> Have question that you would like to see added? Interested in helping curate the data set?</SectionText>
         <SectionText>
-          <strong>Complete the form below and we'll get back to you when we can.</strong>
+          <strong>Complete the form below and we&apos;ll get back to you when we can.</strong>
         </SectionText>
       </SectionNarrow>
 
