d4bab2322bbe1dcb76edf7ce156fabfdf6baa1b5
brandon <bvisokay@gmail.com>
2022-06-10 15:45:12 -0400

    Quick import fix

--- pages/api/uploads/csv.ts
+++ pages/api/uploads/csv.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { getSession } from "next-auth/client"
 import { connectToDatabase } from "../../../lib/db"
-import { Question } from "../../index"
+import { Question } from "../../../lib/types"
 
 /*
 *
