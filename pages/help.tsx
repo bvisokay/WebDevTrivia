a0bf148a0b3f9130565b945a57435f840266ce60
brandon <bvisokay@gmail.com>
2022-06-21 12:56:55 -0400

    Minor change to help page content

--- pages/help.tsx
+++ pages/help.tsx
@@ -1,18 +1,27 @@
 import Link from "next/link"
-import { Section, SectionNarrow } from "../styles/GlobalComponents"
+import { SectionNarrow, SectionTitle, SectionText, SectionDivider } from "../styles/GlobalComponents"
 
 const HelpPage = () => {
   return (
-    <SectionNarrow>
-      <h2>Get Help</h2>
-      <p>
-        Visit the <Link href="/support">support page</Link> and complete the form.
-      </p>
-      <h2>Change Category</h2>
-      <p>From the home screen, hit the settings button, select a catgeory from the dropdown selection.</p>
-      <h2>Change the Number of Questions</h2>
-      <p>Fromm the home screen, hit the settings button, select a number from the dropdown selection.</p>
-    </SectionNarrow>
+    <>
+      <SectionNarrow>
+        <SectionTitle>Change Category</SectionTitle>
+        {/* <SectionDivider /> */}
+        <SectionText>From the home screen, hit the settings button, select a catgeory from the dropdown selection.</SectionText>
+      </SectionNarrow>
+      <SectionNarrow>
+        <SectionTitle>Change the Number of Questions</SectionTitle>
+        {/* <SectionDivider /> */}
+        <SectionText>Fromm the home screen, hit the settings button, select a number from the dropdown selection.</SectionText>
+      </SectionNarrow>
+      <SectionNarrow>
+        <SectionTitle>Get Help</SectionTitle>
+        {/* <SectionDivider /> */}
+        <SectionText>
+          Visit the <Link href="/support">support page</Link> and complete the form.
+        </SectionText>
+      </SectionNarrow>
+    </>
   )
 }
 export default HelpPage
