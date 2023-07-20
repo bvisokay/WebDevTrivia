b70ae42d6b8a74190d7d2853227f5044e37e4e4a
Brandon Visokay <hello@brandonvisokay.com>
2023-07-19 21:32:57 -0400

    Add page guard to /import

--- pages/import.tsx
+++ pages/import.tsx
@@ -1,6 +1,27 @@
 import Import from "../components/Import"
+import { getSession } from "next-auth/react"
+import { GetServerSidePropsContext } from "next"
 
 const ImportPage = () => {
   return <Import />
 }
 export default ImportPage
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession({ req: context.req })
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {
+      session
+    }
+  }
+}
